2dec0af6b329e43e8f203101cea45faa9e77e687
MrNeahka <kucerubov365@gmail.com>
2022-10-06 23:12:41 +0300

    HM-14

--- src/check.js
+++ src/check.js
@@ -1,4 +1,5 @@
 import { findElement, paintCards } from './index.js';
 
-paintCards();
+
 findElement();
+paintCards();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+  let element = document.querySelector('.item');
 
-export const findElement = () => {};
+ while (true){
+   if(element.classList.contains("likedItem")){
+     element.style.background = "blue";
+     break;
+   }
+   element = element.nextElementSibling;
+  }
+};
+
+export const findElement = () => {
+  const massLi = document.querySelectorAll('.item');
+  for(let i = 0; i < massLi.length; i++){
+    if(i % 2 === 0){
+      massLi[i].style.background = "red";
+    }
+  }
+};
